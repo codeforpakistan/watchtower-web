364ba6ef2b935f6511fc5ab5b3eb4da18927ea3c
aliirz <dejavu87@gmail.com>
2025-05-14 09:50:45 +0500

    added message for maintenance

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
-   <div>
-    <Hero/>
-    <Podium/>
-   </div>
+  <div class="maintenance-page">
+    <h1>Site Under Maintenance</h1>
+    <p>We are currently performing scheduled maintenance. Please check back soon.</p>
+  </div>
 </template>
+
+<style scoped>
+.maintenance-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+}
+</style>
